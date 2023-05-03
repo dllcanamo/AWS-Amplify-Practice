c037f9ac4e733d07d8d72fff760114c10d1b8214
dllcanamo <dllcanamo@gmail.com>
2023-05-03 15:07:26 +0800

    added TopNavbar component

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,9 +1,14 @@
-
+import classes from "./top-navbar.module.css"
 
 function TopNavbar() {
     return (
-        <div>
-            hello
+        <div className={classes.topNavbar}>
+            <div className={classes.logoDiv}>logo heres</div>
+            <div className={classes.logoutDiv}>
+                <button className={classes.logoutBtn}>
+                    <p>Logout</p>
+                </button>
+            </div>
         </div>
     )
 }
