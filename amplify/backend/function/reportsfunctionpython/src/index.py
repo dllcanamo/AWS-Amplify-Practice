e70509241074a801e3687c95f69b0315a8635ff4
dllcanamo <dllcanamo@gmail.com>
2023-05-09 01:51:25 +0800

    add reportsfunctionpython

--- amplify/backend/function/reportsfunctionpython/src/index.py
+++ amplify/backend/function/reportsfunctionpython/src/index.py
@@ -1,15 +1,49 @@
 import json
+import boto3
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+    print('received event:')
+    print(event)
+
+    dynamodb = boto3.client('dynamodb')
+
+    if event['httpMethod'] == 'POST':
+        # save item to dynamoDB
+        data = event['body']
+
+        response = dynamodb.put_item(
+            TableName='reportsdb-dev',
+            Item={
+                'uuid': {'S': 'sample-uuid'},
+                'datetime': {'S': 'sample-date'},
+                'description': {'S': 'sample-label'},
+                'path_to_img': {'S': 'sample/path/to/img'}
+            }
+        )
+
+        if response:
+            message = 'item added successfully'
+        else:
+            message = 'there was an error in your request'
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps(message)
+        }
+    else:
+        # get inputs from dynamoDB
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps(event)
+        }
