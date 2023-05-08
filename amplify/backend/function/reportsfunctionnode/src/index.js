7549caa3fcf32c994145eddf163445a503fe5005
dllcanamo <dllcanamo@gmail.com>
2023-05-09 00:52:18 +0800

    add boto3 in dependencies for lambda

--- amplify/backend/function/reportsfunctionnode/src/index.js
+++ amplify/backend/function/reportsfunctionnode/src/index.js
@@ -3,18 +3,40 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const params = {
+  TableName : 'reportsdb',
+  /* Item properties will depend on your application concerns */
+  Item: {
+     uuid: 'sample-uuid',
+     datetime: 'sample-date'
+  }
+}
+
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+    try {
+      await docClient.put(params).promise();
+      return { body: 'Successfully created item!' }
+    } catch (err) {
+      
+      return { error: err }
+    }
+
+// /**
+//  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+//  */
+
+// exports.handler = async (event) => {
+//     console.log(`EVENT: ${JSON.stringify(event)}`);
+//     return {
+//         statusCode: 200,
+//     //  Uncomment below to enable CORS requests
+//     //  headers: {
+//     //      "Access-Control-Allow-Origin": "*",
+//     //      "Access-Control-Allow-Headers": "*"
+//     //  }, 
+//         body: JSON.stringify('Hello from Lambda!'),
+//     };
 };
