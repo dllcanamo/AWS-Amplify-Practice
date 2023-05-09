bc11979d131fcc8d43160c3817a9a142b2e8fdf5
dllcanamo <dllcanamo@gmail.com>
2023-05-09 23:55:04 +0800

    populate download section with ui

--- src/pages/ReportsPage.js
+++ src/pages/ReportsPage.js
@@ -2,7 +2,7 @@ import { useRef, useState, useEffect } from "react";
 import { Storage } from "aws-amplify";
 import { getImageFromS3 } from "../utils/getImages";
 import Modal from 'react-modal';
-import { Image, View, useTheme, Grid, Loader, Button } from "@aws-amplify/ui-react";
+import { Image, View, useTheme, Grid, Loader, Button, Heading, TextField } from "@aws-amplify/ui-react";
 import {
   Table,
   TableCell,
@@ -21,7 +21,6 @@ function ReportsPage() {
     "test2023-05-09T05:50:16.502Z.jpg",
     "test2023-05-09T05:50:16.503Z.jpg",
     "test2023-05-09T05:50:16.505Z.jpg",
-    "test0.jpg",
   ];
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState(null);
@@ -97,7 +96,23 @@ function ReportsPage() {
         <View 
           className="downloadSection"
         >
-          hi
+          <Heading level={5}>Download Reports</Heading>
+          <br></br>
+          {/* should contain a form tag here */}
+          <TextField
+            placeholder="YYYY-MM-DD"
+            label="From:"
+            errorMessage="There is an error"
+            isDisabled={true}
+          />
+          <TextField
+            placeholder="YYYY-MM-DD"
+            label="To:"
+            errorMessage="There is an error"
+            isDisabled={true}
+          />
+          <br></br>
+          <Button disabled={true}>Download as PDF</Button>
         </View>
         <View 
           className="reportsSection"
